1af10705c3563e4bc53416eec4b06c063a9db73c
sadanslargehole <sadan@sadanslargehole.com>
2023-11-06 00:12:07 -0500

    add types for db Models

--- classes/Models.py
+++ classes/Models.py
@@ -10,8 +10,8 @@ class guild(Model):
     enabled = fields.BooleanField()
     whitelist = fields.BooleanField(null=True)
     whitelistedChannels: list = fields. JSONField()
-    blacklistedChannels = fields.JSONField()
-    blacklistedUsers = fields.JSONField()
+    blacklistedChannels: list = fields.JSONField()
+    blacklistedUsers: list = fields.JSONField()
 
     def __str__(self):
         return f'{self.id} {self.canUseBot} {self.unpinChannel} {self.enabled} {self.whitelist} {self.whitelistedChannels} {self.blacklistedChannels} {self.blacklistedUsers}'
